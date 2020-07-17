67f15d4ae6daf382c5288b0900215480e4bdf7ba
pmouhali <p.m.272019@gmail.com>
2020-07-17 13:41:47 +0200

    killme

--- philo_two/elapsed_time.c
+++ philo_two/elapsed_time.c
@@ -18,9 +18,12 @@
 
 unsigned long	elapsed_time(struct timeval t1, struct timeval t2)
 {
-	unsigned long e;
+	unsigned long timestamp1;
+	unsigned long timestamp2;
 
-	e = (t2.tv_sec - t1.tv_sec) * 1000;
-	e += (t2.tv_usec - t1.tv_usec) / 1000;
-	return (e);
+	timestamp1 = (t1.tv_usec / 1000 + t1.tv_sec * 1000);
+	timestamp2 = (t2.tv_usec / 1000 + t2.tv_sec * 1000);
+	if (timestamp1 >= timestamp2)
+		return (0);
+	return (timestamp2 - timestamp1);
 }
